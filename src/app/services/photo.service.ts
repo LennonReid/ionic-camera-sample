d0bc7305519f376de5032a0bb65c7f79fabfaddf
Lennon Reid <lenonrade@163.com>
2023-09-30 23:47:44 +0800

    refactor(app): change the source to CameraSource.Photos in the method of getPhoto()

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -25,13 +25,12 @@ export class PhotoService {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri, // file-based data; provides best performance
-      source: CameraSource.Camera, // automatically take a new photo with the camera
+      source: CameraSource.Photos, // automatically take a new photo with the camera
       quality: 100 // highest quality (0 to 100)
     });
 
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
-    console.log(savedImageFile);
     this.photos.unshift(savedImageFile);
     Preferences.set({
       key: this.PHOTO_STORAGE,
